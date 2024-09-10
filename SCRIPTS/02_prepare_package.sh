5994e422f17cf9bd249d233f0ae6da42de0dc05e
3092099 <122336705+3092099@users.noreply.github.com>
2024-09-10 16:25:11 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -528,7 +528,10 @@ sed -i 's,gc-sections,gc-sections no-lto,g' feeds/packages/net/nginx/Makefile
 # libsodium
 sed -i 's,no-mips16,no-mips16 no-lto,g' feeds/packages/libs/libsodium/Makefile
 
+# 修改内核版本
+sed -i "s/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g" target/linux/x86/Makefile
+
 # 修改默认 IP 地址
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
 #exit 0
